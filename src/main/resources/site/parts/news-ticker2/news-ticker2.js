a1c09f0c93e41e0e05b33b1814b12c359ecd2295
Bobby <bellfalasch@gmail.com>
2015-08-20 16:49:39 +0200

    Added another type of Part, with item-set

---
+++ src/main/resources/site/parts/news-ticker2/news-ticker2.js
@@ -0,0 +1,24 @@
+var portal = require('/lib/xp/portal'); // Import the portal functions
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
+
+// Handle the GET request
+exports.get = function(req) {
+
+	var model = {};
+
+	// Find the current component from request
+	var component = portal.getComponent();
+
+	// Find a config variable for the component
+	var config = component.config;
+
+	model.config = config;
+
+    // Specify the view file to use
+    var view = resolve('news-ticker2.html');
+
+    // Return the response object
+    return {
+        body: thymeleaf.render(view, model)
+    }
+};
