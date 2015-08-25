89cfdcb65862a307fafa95a2b9e49d57e0b0a815
Bobby <bellfalasch@gmail.com>
2015-08-25 14:48:26 +0200

    Correcting view and controller

--- src/main/resources/site/parts/images2/images2.js
+++ src/main/resources/site/parts/images2/images2.js
@@ -11,32 +11,26 @@ exports.get = function(req) {
 	// Find the current component from request
 	var component = portal.getComponent();
 
-	model.config = me.component.config;
+	var config = component.config;
 
 	var imageUrls = new Array();
 	var imageKeys = new Array();
+	var imageUrl = null;
 
-	imageKeys = model.config.image ? UTIL.data.forceArray( model.config.image ) : null;
+	// Add util.forceArray to utilities
+	imageKeys = config.image ? util.forceArray( config.image ) : null;
 
 	if (imageKeys) {
 		for (var i = 0; i < imageKeys.length; i++) {
 			if ( imageKeys[i] ) {
 
-				var images = {}; // Temp object used to store our two image sizes
-
-				images.src = execute('portal.imageUrl', {
+				imageUrl = portal.imageUrl({
 					id: imageKeys[i],
-					filter: "scaleBlock(440,440)",
+					scale: "block(440,440)",
 					format: 'jpg'
 				});
 
-				images.href = execute('portal.imageUrl', {
-					id: imageKeys[i],
-					filter: 'scalewidth(1024)',
-					format: 'jpg'
-				});
-
-				imageUrls.push(images);
+				imageUrls.push(imageUrl);
 			}
 		}
 	}
