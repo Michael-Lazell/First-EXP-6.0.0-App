8b277df053371c879ce728bcda6321d661db7549
Bobby <bellfalasch@gmail.com>
2015-08-25 14:47:42 +0200

    Added forceArray

--- src/main/resources/site/lib/utilities.js
+++ src/main/resources/site/lib/utilities.js
@@ -1,3 +1,10 @@
 exports.log = function (data) {
   log.info('Utilities log %s', JSON.stringify(data, null, 4));
 };
+
+exports.forceArray = function(data) {
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+    return data;
+};
