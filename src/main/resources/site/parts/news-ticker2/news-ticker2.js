19210f1638ac0a371ad7b738a4cde6f01e2348bd
Bobby <bellfalasch@gmail.com>
2015-08-26 14:51:35 +0200

    Added logging

--- src/main/resources/site/parts/news-ticker2/news-ticker2.js
+++ src/main/resources/site/parts/news-ticker2/news-ticker2.js
@@ -1,6 +1,8 @@
 var portal = require('/lib/xp/portal'); // Import the portal functions
 var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
 
+var util = require('/lib/utilities'); // My own extension functions
+
 // Handle the GET request
 exports.get = function(req) {
 
@@ -13,6 +15,8 @@ exports.get = function(req) {
 	var config = component.config;
 
 	model.config = config;
+
+	//util.log(config);
 
     // Specify the view file to use
     var view = resolve('news-ticker2.html');
